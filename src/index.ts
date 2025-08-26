f3a7d3ac07829a563f721c8749f2c6f5a1954ea9
Michal <92219340+s24407-pj@users.noreply.github.com>
2025-08-26 16:23:29 +0200

    Change redirect URL and status code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
 	async fetch(request): Promise<Response> {
-		const destinationURL = "https://example.com";
-		const statusCode = 301;
+		const destinationURL = "https://kacosmetology.pl";
+		const statusCode = 302;
 		return Response.redirect(destinationURL, statusCode);
 	},
 } satisfies ExportedHandler;
